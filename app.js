66836797419c89839dc72b8674efb6f124bdf4f5
Jarrell Mikko Romero <jarrellmikkoromero@gmail.com>
2024-08-04 22:16:21 +0800

    Revert "Create another version using ejs"
    This reverts commit abdb54d3cc8b181c611dd7b5fae07b10a57ec00a.

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 var createError = require("http-errors");
-const express = require("express");
-const path = require("node:path");
-const indexRouter = require("./routes/indexRouter");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const assetsPath = path.join(__dirname, "public");
-const app = express();
-const PORT = 3000;
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 
-// add middlewares
 app.use(logger("dev"));
-app.use(express.static(assetsPath));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-// add router middlewares
-
-// add routers
 app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -26,16 +28,14 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function error(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error", { error: err });
+  res.render("error");
 });
 
-app.listen(PORT, () =>
-  console.log(`My first Express app - listening on port ${PORT}!`)
-);
+module.exports = app;
